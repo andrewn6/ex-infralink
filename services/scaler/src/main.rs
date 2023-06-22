4b9f7d720c61f616adc9d7a9fb8750ab72f3fd61
andrew <andrewnijmeh1@gmail.com>
2023-06-21 21:16:17 -0400

    (scaler): manual scaling done

--- services/scaler/src/main.rs
+++ services/scaler/src/main.rs
@@ -4,7 +4,6 @@ use serde::{Deserialize};
 use tokio::time::{sleep, Duration};
 use std::env;
 use std::error::Error;
-use std::net::SocketAddr;
 use shiplift::{Docker, ContainerListOptions, ContainerOptions};
 
 use warp::{Filter, reject};
@@ -48,6 +47,9 @@ async fn track_container_stats() -> Result<(), Box<dyn Error>> {
 	loop {
 		let containers = docker.containers().list(&options).await?;
 
+		let cpu_usage_sum = 0.0;
+		let memory_usage_sum = 0.0;
+
 		for container in containers {
 			let stats = docker.containers().get(&container.id).stats().await?;
 
@@ -59,12 +61,31 @@ async fn track_container_stats() -> Result<(), Box<dyn Error>> {
 			}
 			if let Some(memory_stats) = stats.memory_stats {
 				let memory_usage = memory_stats.usage as f64;
-				println!("Memory usage: {} bytes". memory_usage);
+				println!("Memory usage: {} bytes", memory_usage);
 			}
+		}
+
+		let avg_cpu_usage = cpu_usage_sum / containers.len() as f64;
+		let avg_memory_usage = memory_usage_sum / containers.len() as f64;
+
+		println!("Average CPU usage: {}%", avg_cpu_usage);
+		println!("Average memory usage: {} bytes", avg_memory_usage);
+
+		if avg_cpu_usage > 70.0 || avg_memory_usage > 100_000_000.0 {
+			scale();
 		}
 
 		sleep(Duration::from_secs(60)).await;
 	}
+}
+
+
+async fn scale() {
+	for _ in 0..CONTAINER_THRESHOLD {
+		// atm im not sure what image we should be using here
+		create_container("ubuntu:latest").await.expect("Failed to create new containers to scale up");
+	}
+	println!("Successfuly scaled up containers by {}", CONTAINER_THRESHOLD);
 }
 
 fn calculate_cpu_percent(cpu_stats: &shiplift::rep::CpuStats) -> f64 {
@@ -119,17 +140,23 @@ async fn main() -> Result<(), dyn Error> {
 	let health_check_route = warp::path("health")
         .and(warp::get())
         .map(warp::reply);
+
 	let hello_route = warp::path("hello")
     	.and(warp::get())
     	.and(warp::path::end())
     	.and(warp::any().map(|| "Hello, World!"));
+
+	let scale_route = warp::path("scale")
+		.and(warp::get())
+		.and(warp::path::end())
+		.and(warp::any().map(scale));
 
 	let routes = health_check_route
 		.or(hello_route)
 		.or(prometheus_metrics);
 
 	let server_address = ([127, 0, 0, 1], 8087);
-    println!("Server running at http://localhost:8087");
+    println!("Scaler server running at http://localhost:8087");
 
     warp::serve(routes).run(server_address).await;
 

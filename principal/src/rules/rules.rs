ea157246fcf794dc0dee25f939b7aee53283431d
andrew <andrewnijmeh1@gmail.com>
2023-07-02 13:37:10 -0400

    (principal): migrate rules to cockroach db instead of yaml

--- principal/src/rules/rules.rs
+++ principal/src/rules/rules.rs
@@ -1,19 +1,53 @@
-use serde_yaml::from_reader;
-use std::fs::File;
-use std::io::BufReader;
-use serde::{Deserialize, Serialize};
+use anyhow::Result;
+use sqlx::postgres::PgPoolOptions;
+use dotenv_codegen::dotenv;
 
-/* Used for pre-warmed instance rules */
-#[derive(Debug, Deserialize, Serialize)]
-pub struct Rule {
-    pub provider: String,
-    pub regions: Vec<String>,
-    pub instance_count: usize,
-}
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let database_url  = dotenv!("COCKROACH_DB_URL");
+    let pool = PgPoolOptions::new().connect(database_url).await?;
 
-pub fn load_rules() -> Vec<Rule> {
-    let file = File::open("../../config/principal/prewarm_rules.yaml").unwrap();
-    let reader = BufReader::new(file);
-    let rules: Vec<Rule> = from_reader(reader).unwrap();
-    rules
+    sqlx::query(
+        r#"
+        CREATE TABLE IF NOT EXISTS Providers (
+            id SERIAL PRIMARY KEY,
+            provider TEXT NOT NULL,
+            region TEXT NOT NULL,
+            instance_count INT NOT NULL,
+            UNIQUE(provider, region)
+        )
+        "#,
+    )    
+    .execute(&pool)
+    .await?;
+
+    let providers = vec![
+        ("vultr", "us-west", 1),
+        ("vultr", "us-east", 1),
+        ("host_hatch", "eu-west", 1),
+    ];
+
+    for (provider, region, instance_count) in providers {
+        let result = sqlx::query(
+            r#"
+            INSERT INTO Providers (provider, region, instance_count)
+            VALUES ($1, $2, $3)
+            ON CONFLICT (provider, region) DO NOTHING
+            "#,
+        )
+        .bind(provider)
+        .bind(region)
+        .bind(instance_count)
+        .execute(&pool)
+        .await;
+
+        match result {
+            Ok(_) => println!("Data inserted or already exists."),
+            Err(err) => {
+                println!("Failed to insert data: {}", err);
+            }
+        }
+    }
+
+    Ok(())
 }

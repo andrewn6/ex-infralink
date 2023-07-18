95e64413f5433d55bb4e1d1ff88147d5c0b2a1b7
andrew <andrewnijmeh1@gmail.com>
2023-07-17 22:11:20 -0400

    (worker): basic implementation finished

--- worker/src/healer/healer.rs
+++ worker/src/healer/healer.rs
@@ -3,11 +3,12 @@ mod healer {
 }
 
 use std::time::Duration;
+use std::sync::Arc;
 
 use futures_util::StreamExt;
 use tonic::{Request, Response, Status};
-use tonic::transport::Server;
 use tokio::time;
+use tokio::sync::Mutex;
 
 use bollard::container::{CreateContainerOptions, RemoveContainerOptions, ListContainersOptions};
 use bollard::Docker;
@@ -18,11 +19,12 @@ use healer::StartHealingResponse;
 use healer::StopHealingRequest;
 use healer::StopHealingResponse;
 
+#[derive(Clone)]
 pub struct MyHealer {
     docker: Docker,
     create_options: CreateContainerOptions<String>,
     container_config: bollard::container::Config<String>,
-    healing: bool,
+    healing: Arc<Mutex<bool>>,
 }
 
 impl MyHealer {
@@ -31,14 +33,15 @@ impl MyHealer {
             docker,
             create_options,
             container_config,
-            healing: false,
+            healing: Arc::new(Mutex::new(false)),
         }
     }
 
-    pub async fn start(&mut self) {
-        self.healing = true;
+    pub async fn start(self) {
+        let mut healing = self.healing.lock().await;
+        *healing = true;
 
-        while self.healing {
+        while *self.healing.lock().await {
             let containers = self.docker.list_containers(Some(ListContainersOptions::<String>{
                 all: true,
                 ..Default::default()
@@ -65,8 +68,9 @@ impl MyHealer {
         }
     }
 
-    pub fn stop(&mut self) {
-        self.healing = false;
+    pub async fn stop(self) {
+        let mut healing = self.healing.lock().await;
+        *healing = false;
     }
 }
 
@@ -76,7 +80,12 @@ impl Healer for MyHealer {
         &self,
         _request: Request<StartHealingRequest>,
     ) -> Result<Response<StartHealingResponse>, Status> {
-        self.start().await;
+        let healer = self.clone();
+
+        tokio::spawn(async move {
+            healer.start().await;
+        });
+
         Ok(Response::new(StartHealingResponse {
             message: "Healing process started".to_string()
         }))
@@ -86,10 +95,23 @@ impl Healer for MyHealer {
         &self,
         _request: Request<StopHealingRequest>,
     ) -> Result<Response<StopHealingResponse>, Status> {
-        self.stop();
+        let healer = self.clone();
+
+        tokio::spawn(async move {
+            healer.stop().await;
+        });
+
         Ok(Response::new(StopHealingResponse {
             message: "Healing process stopped".to_string()
         }))
     }
 
+    async fn heal(
+        &self,
+        _request: Request<healer::HealRequest>,
+    ) -> Result<Response<healer::HealResponse>, Status> {
+        Ok(Response::new(healer::HealResponse {
+            message: "Healing process started".to_string()
+        }))
+    }
 }
